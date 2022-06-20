afc9e5a4d9ac13115f6a32b85f38429ecdcd8d69
geonya <geony8410@gmail.com>
2022-06-20 09:30:30 +0900

    seeCafes offset

--- src/cafes/seeCafes/seeCafes.resolvers.ts
+++ src/cafes/seeCafes/seeCafes.resolvers.ts
@@ -4,10 +4,10 @@ import { protectedResolver } from '../../users/users.utils';
 
 export const resolvers: Resolvers = {
 	Query: {
-		seeCafes: protectedResolver(async (_, { page }, { client }) =>
+		seeCafes: protectedResolver(async (_, { offset }, { client }) =>
 			client.cafe.findMany({
 				take: CAFES_PER_PAGE,
-				skip: page <= 0 ? 0 : (page - 1) * CAFES_PER_PAGE,
+				skip: offset <= 0 ? 0 : (offset - 1) * CAFES_PER_PAGE,
 				orderBy: { createdAt: 'desc' },
 			})
 		),
--- src/users/users.resolvers.ts
+++ src/users/users.resolvers.ts
@@ -2,13 +2,15 @@ import { Resolvers } from '../types';
 
 export const resolvers: Resolvers = {
 	User: {
-		countCafes: async ({ id }, _: any, { client }) =>
+		cafes: ({ id }, _: any, { client }) =>
+			client.cafe.findMany({ where: { userId: id } }),
+		countCafes: ({ id }, _: any, { client }) =>
 			client.cafe.count({ where: { userId: id } }),
-		givenLikes: async ({ id }, _: any, { client }) =>
+		givenLikes: ({ id }, _: any, { client }) =>
 			client.like.count({ where: { cafe: { userId: id } } }),
-		totalFollowing: async ({ id }, _: any, { client }) =>
+		totalFollowing: ({ id }, _: any, { client }) =>
 			client.user.count({ where: { followers: { some: { id } } } }),
-		totalFollowers: async ({ id }, _: any, { client }) =>
+		totalFollowers: ({ id }, _: any, { client }) =>
 			client.user.count({ where: { following: { some: { id } } } }),
 	},
 };
