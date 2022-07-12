7ff69a57e5ec9fdc0540652ec43c186b519138ed
geonya <geony8410@gmail.com>
2022-07-12 11:29:27 +0900

    user address

--- src/users/editProfile/editProfile.typeDefs.ts
+++ src/users/editProfile/editProfile.typeDefs.ts
@@ -1,16 +1,16 @@
-import { gql } from "apollo-server-express";
+import { gql } from 'apollo-server-express';
 
 export default gql`
-	type Mutation {
-		editProfile(
-			username: String
-			name: String
-			email: String
-			password: String
-			avatar: Upload
-			githubUsername: String
-			location: String
-		): MutationResponse
-	}
-	scalar Upload
+  type Mutation {
+    editProfile(
+      username: String
+      name: String
+      email: String
+      password: String
+      avatar: Upload
+      githubUsername: String
+      address: String
+    ): MutationResponse
+  }
+  scalar Upload
 `;
--- src/users/users.typeDefs.ts
+++ src/users/users.typeDefs.ts
@@ -1,24 +1,24 @@
 import { gql } from 'apollo-server-express';
 
 export default gql`
-	type User {
-		id: Int!
-		username: String!
-		email: String!
-		name: String!
-		password: String!
-		location: String
-		avatarUrl: String
-		githubUsername: String
-		following: [User]
-		totalFollowing: Int!
-		followers: [User]
-		totalFollowers: Int!
-		cafes: [Cafe]
-		countCafes: Int!
-		givenLikes: Int!
-		photos: [CafePhoto]
-		createdAt: String!
-		updatedAt: String!
-	}
+  type User {
+    id: Int!
+    username: String!
+    email: String!
+    name: String!
+    password: String!
+    address: String
+    avatarUrl: String
+    githubUsername: String
+    following: [User]
+    totalFollowing: Int!
+    followers: [User]
+    totalFollowers: Int!
+    cafes: [Cafe]
+    countCafes: Int!
+    givenLikes: Int!
+    photos: [CafePhoto]
+    createdAt: String!
+    updatedAt: String!
+  }
 `;
