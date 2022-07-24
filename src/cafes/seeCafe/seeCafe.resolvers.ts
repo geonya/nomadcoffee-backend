ef6c48b681e1dfd0bb6577fcd48d401cc8e05d02
geonya <geony8410@gmail.com>
2022-07-24 17:08:34 +0900

    seecafes inititescroll false

--- src/cafes/seeCafe/seeCafe.resolvers.ts
+++ src/cafes/seeCafe/seeCafe.resolvers.ts
@@ -4,9 +4,7 @@ import { protectedResolver } from '../../users/users.utils';
 export const resolvers: Resolvers = {
   Query: {
     seeCafe: protectedResolver(async (_, { id }, { client }) => {
-      console.log(id);
       const cafe = await client.cafe.findUnique({ where: { id } });
-      console.log(cafe);
       return cafe;
     }),
   },
--- src/cafes/seeCafes/seeCafes.resolvers.ts
+++ src/cafes/seeCafes/seeCafes.resolvers.ts
@@ -1,13 +1,10 @@
-import { CAFES_PER_ITEMS } from '../../constant';
 import { Resolvers } from '../../types';
 import { protectedResolver } from '../../users/users.utils';
 
 export const resolvers: Resolvers = {
   Query: {
-    seeCafes: protectedResolver(async (_, { offset }, { client }) =>
+    seeCafes: protectedResolver(async (_, __, { client }) =>
       client.cafe.findMany({
-        take: CAFES_PER_ITEMS,
-        skip: offset,
         orderBy: { createdAt: 'desc' },
       })
     ),
