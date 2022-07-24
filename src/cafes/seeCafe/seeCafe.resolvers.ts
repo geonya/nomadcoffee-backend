969f1415dd6b643ed644112a8dca46df44791eca
geonya <geony8410@gmail.com>
2022-07-24 16:12:51 +0900

    deploy cafe test

--- src/cafes/seeCafe/seeCafe.resolvers.ts
+++ src/cafes/seeCafe/seeCafe.resolvers.ts
@@ -2,9 +2,12 @@ import { Resolvers } from '../../types';
 import { protectedResolver } from '../../users/users.utils';
 
 export const resolvers: Resolvers = {
-	Query: {
-		seeCafe: protectedResolver(async (_, { id }, { client }) =>
-			client.cafe.findUnique({ where: { id } })
-		),
-	},
+  Query: {
+    seeCafe: protectedResolver(async (_, { id }, { client }) => {
+      console.log(id);
+      const cafe = await client.cafe.findUnique({ where: { id } });
+      console.log(cafe);
+      return cafe;
+    }),
+  },
 };
